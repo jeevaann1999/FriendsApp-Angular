863e5e1cfd6074d6adde6356ba42f81bec7886d8
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-26 11:43:45 +0530

    added nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddFriendsComponent } from './add-friends/add-friends.component';
 import { ViewFriendsComponent } from './view-friends/view-friends.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddFriendsComponent
+  },
+  {
+    path:"view",
+    component:ViewFriendsComponent
+  },
+  {
+    path:"search",
+    component:
+  },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddFriendsComponent,
-    ViewFriendsComponent
+    ViewFriendsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
